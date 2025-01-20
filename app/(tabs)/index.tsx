9c57d34df0c1c57a3ee653c5dc5b0c4b4cf53aad
risyad <risyadzyn@gmail.com>
2025-01-20 21:37:11 +0700

    docs: get pokemon list from api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,33 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Pokemon {
+  name: string;
+  url: string;  
+}
+
+interface PokemonList {
+  datas: Pokemon[];
+}
 
 export default function TabOneScreen() {
+  const [datas, setDatas] = useState<PokemonList>({ datas: [] });
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/').then((res) => {
+      setDatas({ datas: res.data.results });
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      {datas.datas.map((data) => (
+        <Text key={data.name}>{data.name}</Text>
+      ))}
+      {/* <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
